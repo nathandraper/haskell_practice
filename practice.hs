b31a597016005394dbaf6e03c10810580f4ec119
Nathan Draper <nathanjcdraper@gmail.com>
2019-06-23 09:08:40 -0400

    complete chapter 5

--- practice.hs
+++ practice.hs
@@ -1,3 +1,4 @@
+import Data.Char
 -- beginning practice
 {-
     nested
@@ -209,5 +210,94 @@ luhndouble x = if x*2 <=9 then x*2 else x*2 - 9
 luhn :: Int -> Int -> Int -> Int -> Bool
 luhn a b c d = if (luhndouble a + b + luhndouble c + d) `mod` 10 == 0 then True else False
 
+-- ch. 5
+-- list comprehensions
+-- [x | x -> xs]
+-- [(x, y) | x <- [1..3], y <- y[4..5]] all combinations of x and y
 
+length2 :: [a] -> Int
+length2 xs = sum [1 | _ <- xs]
 
+firsts :: [[a]] -> [a]
+firsts xs = [x | [x, _] <- xs]
+
+-- guards
+factors :: Int -> [Int]
+factors n = [x | x <- [1..n], n `mod` x == 0]
+
+prime :: Int -> Bool
+prime n = factors n == [1, n]
+
+primes :: Int -> [Int]
+primes n = [x | x <- [1..n], prime x]
+
+find :: Eq a => a -> [(a, b)] -> [b]
+find x kvs = [v | (k, v) <- kvs, k == x]
+
+-- zip
+pairs :: [a] -> [b] -> [(a,b)]
+pairs xs ys = zip xs ys
+
+positions :: Eq a => a -> [a] ->  [Int]
+positions x xs = [i | (i, v) <- (zip [0..] xs), v == x]
+
+-- string comprehensions (polymorphic functions work on strings as well)
+lowers :: String -> Int
+lowers s = sum [1 | c <- s, c >= 'a' && c <= 'z']
+
+count :: Char -> String -> Int
+count c s = sum [1 | c' <- s, c' == c] 
+
+let2int :: Char -> Int
+let2int c = ord c - ord 'a'
+
+int2let :: Int -> Char
+int2let n = chr (n + ord 'a')
+
+islower :: Char -> Bool
+islower c = c >= 'a' && c <= 'z'
+
+shift :: Char -> Int -> Char
+--shift c n = if islower c then int2let ((let2int c + n) `mod` 25) else c
+shift c n | islower c = int2let ((let2int c + n) `mod` 25)
+          | otherwise = c
+
+encode :: String -> Int -> String
+encode s n = [shift c n | c  <- s]
+
+-- exercises
+-- 1
+sumsquare :: Int
+sumsquare = sum [x^2 | x <-[0..100]]
+
+-- 2
+grid :: Int -> Int -> [(Int, Int)]
+grid x y = [(x', y') | x' <- [0..x], y' <- [0..y]]
+
+-- 3 
+square :: Int -> [(Int, Int)]
+square n = [(x, y) | (x, y) <- grid n n, not (x == y)]
+
+-- 4
+replicate2 :: a -> Int -> [a]
+replicate2 x n = [x | _ <- [1..n]]
+
+-- 5
+pyths :: Int -> [(Int, Int, Int)]
+pyths n = [(a, b, c) | a <- [1..n], b <- [1..n], c <- [1..n], a^2 + b^2 == c^2]
+
+-- 6
+perfects :: Int -> [Int]
+perfects n = [n | n <- [1..n], sum (factors n) - n == n]
+
+-- 7
+pairsConcat :: [a] -> [b] -> [(a, b)]
+pairsConcat xs ys = concat [[(x, y) | y <- ys] | x <- xs ]
+
+-- 8
+positionsFind :: Eq a => a -> [a] -> [Int]
+positionsFind x xs = [i | i <- find x (zip xs [0..])]
+
+--9
+scalarProduct :: [Int] -> [Int] -> Int
+scalarProduct xs ys = sum [x*y | (x, y) <- zip xs ys]
